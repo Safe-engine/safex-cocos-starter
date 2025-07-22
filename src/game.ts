5217cde4bffac2bd2af3d901a0291426c7f19f9c
antn1 <antn@ftech.com.vn>
2025-07-22 17:41:28 +0700

    update startGame

--- src/game.ts
+++ src/game.ts
@@ -1,22 +1,11 @@
-import { initWorld, loadAll, loadScene, startGame } from '@safe-engine/cocos'
+import { loadAll, loadScene, startGame } from '@safe-engine/cocos'
 
 import { defaultFont, sf_progress_bar } from './assets'
 import { Loading } from './scene/Loading'
 import { designedResolution } from './settings'
 
-startGame(
-  {
-    debugMode: 1,
-    showFPS: false,
-    frameRate: 60,
-    id: 'gameCanvas',
-    renderMode: 1,
-  },
-  designedResolution,
-  () => {
-    initWorld(defaultFont)
-    loadAll([sf_progress_bar], (p) => {
-      if (p >= 1) loadScene(Loading)
-    })
-  },
-)
+startGame(defaultFont, designedResolution).then(() => {
+  loadAll([sf_progress_bar], (p) => {
+    if (p >= 1) loadScene(Loading)
+  })
+})
