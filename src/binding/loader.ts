5bd003127f5aa3746a60d336ecb7184c91c82cd8
antn1 <antn@ftech.com.vn>
2025-10-10 09:01:59 +0700

    update loader

--- src/binding/loader.ts
+++ src/binding/loader.ts
@@ -3,8 +3,9 @@ import { loadAll } from '@safe-engine/cocos'
 import * as FontAssets from '../assets/FontAssets'
 import * as TextureAssets from '../assets/TextureAssets'
 
-export function loadAssets(cb: (progress: number) => void, onCompleted: () => void) {
+export async function loadAssets(cb: (progress: number) => void, onCompleted: () => void) {
   // load the texture we need
   const assets = [...Object.values(TextureAssets), ...Object.values(FontAssets)]
-  loadAll(assets, cb, onCompleted)
+  await loadAll(assets, cb)
+  onCompleted()
 }
