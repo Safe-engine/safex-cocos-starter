5cae2643490a677f1b3738470b9d8e82bff58008
antn1 <antn@ftech.com.vn>
2026-01-29 15:33:54 +0700

    fix loading

--- src/game.ts
+++ src/game.ts
@@ -1,9 +1,10 @@
-import { loadScene, startGame } from '@safe-engine/cocos'
+import { loadAll, loadScene, startGame } from '@safe-engine/cocos'
 
-import { lilita_one_regularFont } from './assets'
+import { lilita_one_regularFont, sf_progress_bar, sf_progress_bg } from './assets'
 import Loading from './scene/Loading'
 import { designedResolution } from './settings'
 
-startGame(lilita_one_regularFont, designedResolution).then(() => {
+startGame(lilita_one_regularFont, designedResolution).then(async () => {
+  await loadAll([sf_progress_bar, sf_progress_bg])
   loadScene(Loading)
 })
