bbac377b0685c27dc9cb778b5d241b95725193b6
antn1 <antn@ftech.com.vn>
2026-06-23 09:37:25 +0700

    fix template

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,18 +1,17 @@
-import path from "node:path";
 import { defineConfig } from "vite";
 import { sdlTsxTransform } from './vite-plugin-sdl-tsx';
 
 export default defineConfig({
-  root: __dirname,
-  publicDir: path.resolve(__dirname, "../res"),
+  root: 'web',
+  publicDir: "../res",
   resolve: {
     alias: {
-      box2d: path.resolve(__dirname, "./box2d.ts"),
-      sdl3: path.resolve(__dirname, "./sdl3.ts"),
+      box2d: "/box2d.ts",
+      sdl3: "/sdl3.ts",
     },
   },
   build: {
-    outDir: path.resolve(__dirname, "../dist"),
+    outDir: "../dist",
     emptyOutDir: true,
     target: "es2020",
   },
